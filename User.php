9eefed814e9b8d944d08b324d5135b3a27806ffa
Avaiap <rozhkov.danila@mail.ru>
2023-06-23 00:04:08 +0300

    Пользователь

--- User.php
+++ User.php
@@ -1,12 +1,25 @@
 <?php
 class User{
-    protected string $login;
+    private string $login;
     private int $password;
 
-    public function __construct(string $login, int $password)
-    {
+    public function __construct(string $login, int $password) {
         $this->login = $login;
         $this->password = $password;
     }
+
+    public function setPassword(int $password): void {
+        $this->password = $password;
+    }
+    public function setLogin(string $login): void {
+        $this->login = $login;
+    }
+
+    public function getPassword(int $password) {
+        return $this->password;
+    }
+
+    public function getLogin(string $login) {
+        return $this->login;
+    }
 }
-//
