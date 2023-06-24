cb6214f18dbbf3ee2bdcd5e4f16adb51791d6d42
Avaiap <rozhkov.danila@mail.ru>
2023-06-24 15:56:45 +0300

    Корзина пользователя

--- Basket.php
+++ Basket.php
@@ -3,14 +3,27 @@ class Basket{
     private array $basket;
     private string $userName;
 
+    public function __construct(string $userName)
+    {
+        $this->userName = $userName;
+    }
+
     public function addToBasket(Product $nameProduct): void {
         $this->basket[] = $nameProduct;
     } 
 
     public function printProductsInTheBasketInfo(): void {
+        echo "Корзина пользователя : $this->userName" . "<br>" . "<br>";
         foreach ($this->basket as $category => $product) {
-            echo "Ключ: " . $category . "<br>" . "<br>" . $product->printInfo() . "<br>";
-            echo "Корзина пользователя : $this->userName" . "<br>";
+            echo "Ключ: " . $category . "<br>" . $product->printInfo() . "<br>";
         }
+    }
+
+    public function setUserName(string $userName): void {
+        $this->userName = $userName;
+    }
+
+    public function getUserName(string $userName) {
+        return $this->userName;
     }
 }
